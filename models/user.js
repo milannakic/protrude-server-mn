d7acf8a607aa00edcead80c14e8a19df7a568e04
Milan Nakic <milan.nakic@gmail.com>
2020-08-06 13:49:27 +0200

    Added and configured bcrypt for user password hashing

--- models/user.js
+++ models/user.js
@@ -21,6 +21,28 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+UserSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    let hashPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.method.comparePassword = async function (candidatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
