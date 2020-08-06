5a795aeab9b3693070ce09bbce7b5c6be05ab255
Milan Nakic <milan.nakic@gmail.com>
2020-08-06 13:37:16 +0200

    Added user model and DB connection

---
+++ models/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.Promise = Promise; //needed for ES2017 methods
+
+const uri = process.env.DATABASEURL;
+
+/* const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect(uri, options).then(
+  () => {},
+  (err) => {
+    console.log(err);
+    var d = Date(Date.now()).toString();
+    console.log.call(console, d);
+  }
+); */
+
+mongoose.connect(uri, {
+  keepAlive: true,
+  useMongoClient: true,
+});
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
