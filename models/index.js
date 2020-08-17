3da236f2730350c50558640dc55f7e7abd7e1d7c
Milan Nakic <milan.nakic@gmail.com>
2020-08-17 21:54:21 +0200

    Removed some comments from mongoose models

--- models/index.js
+++ models/index.js
@@ -1,30 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
-mongoose.Promise = Promise; //needed for ES2017 methods
+mongoose.Promise = Promise;
 
 const uri = process.env.DATABASEURL;
-
-//OLD DB connection config
-/* const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: false, // Don't build indexes
-  poolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
-}; */
-
-/* mongoose.connect(uri, options).then(
-  () => {},
-  (err) => {
-    console.log(err);
-    var d = Date(Date.now()).toString();
-    console.log.call(console, d);
-  }
-); */
 
 const options = {
   useNewUrlParser: true,
